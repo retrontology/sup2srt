a7f35b0e4ddd2d8349cfc5a983cc38748c461fea
Retrontology <retrontology@hotmail.com>
2020-11-07 17:44:15 -0600

    added bitmap skellington

---
+++ src/bmp/bitmap.cpp
@@ -0,0 +1,18 @@
+/*
+ * bitmap.cpp
+ *
+ *  Created on: Nov 7, 2020
+ *      Author: blazer
+ */
+
+#include "bitmap.h"
+
+bitmap::bitmap() {
+	// TODO Auto-generated constructor stub
+
+}
+
+bitmap::~bitmap() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/bmp/bitmap.h
@@ -0,0 +1,17 @@
+/*
+ * bitmap.h
+ *
+ *  Created on: Nov 7, 2020
+ *      Author: blazer
+ */
+
+#ifndef SRC_BMP_BITMAP_H_
+#define SRC_BMP_BITMAP_H_
+
+class bitmap {
+public:
+	bitmap();
+	virtual ~bitmap();
+};
+
+#endif /* SRC_BMP_BITMAP_H_ */
