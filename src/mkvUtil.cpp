4faa66bdedbfa80d57f55820590ec98e6049ebe6
Retrontology <retrontology@hotmail.com>
2020-11-19 02:05:39 -0600

    added multistream

--- src/mkvUtil.cpp
+++ src/mkvUtil.cpp
@@ -7,11 +7,23 @@
 
 #include "mkvUtil.h"
 #include "pgs/pgsUtil.h"
-extern "C"
+
+
+mkvPGS::mkvPGS()
 {
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
+	this->data = std::stringstream();
+	this->index = 0;
+	this->language = std::string("");
 }
+
+mkvPGS::mkvPGS(unsigned int index, std::string language)
+{
+	this->data = std::stringstream();
+	this->index = index;
+	this->language = language;
+}
+
+mkvPGS::~mkvPGS(){};
 
 
 void mkvUtil::tsToChar4(char * buffer, u_int32_t ts)
@@ -33,26 +45,60 @@ std::stringstream mkvUtil::extractMKVsup(std::string filename, int index)
 	{
 		if (packet->stream_index == index)
 		{
-			int offset = 0;
-			while(offset < packet->size)
-			{
-				unsigned int segSize = pgsUtil::char2ToInt(reinterpret_cast<char *>(packet->data + offset + 1));
-				out << "PG";
-				char * buffer = new char[4];
-				u_int32_t pts = packet->pts * 90;
-				u_int32_t dts = packet->dts * 90;
-				tsToChar4(buffer, pts);
-				out << std::string(buffer, 4);
-				tsToChar4(buffer, dts);
-				out << std::string(buffer, 4);
-				delete[] buffer;
-				out << std::string(reinterpret_cast<char *>(packet->data + offset), segSize + 3);
-				offset += 3 + segSize;
-			}
+			out << mkvUtil::formatPacket(packet);
 		}
 		av_packet_unref(packet);
 	}
 	avformat_close_input(&mkvFile);
 	av_packet_free(&packet);
 	return out;
+}
+
+std::map<unsigned int, std::stringstream> mkvUtil::extractAllMKVsup(std::string filename)
+{
+	std::map<unsigned int, std::stringstream> streams;
+	AVFormatContext * mkvFile = avformat_alloc_context();
+	avformat_open_input(&mkvFile, filename.c_str(), NULL, NULL);
+	avformat_find_stream_info(mkvFile,  NULL);
+	for(int i = 0; i < mkvFile->nb_streams; i++)
+	{
+		if(mkvFile->streams[i]->codecpar->codec_id == AV_CODEC_ID_HDMV_PGS_SUBTITLE)
+		{
+			streams.insert(std::pair<int, std::stringstream>(i, std::stringstream()));
+		}
+	}
+	AVPacket * packet = av_packet_alloc();
+	while (av_read_frame(mkvFile, packet) >= 0)
+	{
+		if (mkvFile->streams[packet->stream_index]->codecpar->codec_id == AV_CODEC_ID_HDMV_PGS_SUBTITLE)
+		{
+			streams[packet->stream_index] << mkvUtil::formatPacket(packet);
+		}
+		av_packet_unref(packet);
+	}
+	avformat_close_input(&mkvFile);
+	av_packet_free(&packet);
+	return streams;
+}
+
+std::string mkvUtil::formatPacket(AVPacket* packet)
+{
+	std::ostringstream out;
+	int offset = 0;
+	while(offset < packet->size)
+	{
+		unsigned int segSize = pgsUtil::char2ToInt(reinterpret_cast<char *>(packet->data + offset + 1));
+		out << "PG";
+		char * buffer = new char[4];
+		u_int32_t pts = packet->pts * 90;
+		u_int32_t dts = packet->dts * 90;
+		tsToChar4(buffer, pts);
+		out << std::string(buffer, 4);
+		tsToChar4(buffer, dts);
+		out << std::string(buffer, 4);
+		delete[] buffer;
+		out << std::string(reinterpret_cast<char *>(packet->data + offset), segSize + 3);
+		offset += 3 + segSize;
+	}
+	return out.str();
 }
--- src/mkvUtil.h
+++ src/mkvUtil.h
@@ -9,10 +9,29 @@
 #define SRC_MKVUTIL_H_
 
 #include <sstream>
+#include <map>
+#include <string>
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+}
+
+struct mkvPGS
+{
+	std::stringstream data;
+	std::string language;
+	unsigned int index;
+	mkvPGS();
+	mkvPGS(unsigned int index, std::string language);
+	virtual ~mkvPGS();
+};
 
 namespace mkvUtil {
 	void tsToChar4(char * buffer, u_int32_t ts);
 	std::stringstream extractMKVsup(std::string filename, int index);
+	std::map<unsigned int, std::stringstream> extractAllMKVsup(std::string filename);
+	std::string formatPacket(AVPacket* packet);
 };
 
 #endif /* SRC_MKVUTIL_H_ */
